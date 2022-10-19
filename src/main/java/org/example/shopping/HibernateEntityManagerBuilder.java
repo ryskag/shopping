385ea6b85e632e8ee24a46682b6f257e51a951ba
Rysard <rysard.gulbinovic@gmail.com>
2022-10-19 21:09:56 +0300

    Extracted entityManager building.

---
+++ src/main/java/org/example/shopping/HibernateEntityManagerBuilder.java
@@ -0,0 +1,33 @@
+package org.example.shopping;
+
+import org.example.shopping.db.entity.Order;
+import org.example.shopping.db.entity.OrderItem;
+import org.example.shopping.db.entity.Product;
+import org.example.shopping.db.entity.Review;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import javax.persistence.EntityManager;
+
+public class HibernateEntityManagerBuilder {
+
+    public static EntityManager build() {
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml")
+                .build();
+
+        MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+        metadataSources.addAnnotatedClass(Order.class);
+        metadataSources.addAnnotatedClass(Review.class);
+        metadataSources.addAnnotatedClass(Product.class);
+        metadataSources.addAnnotatedClass(OrderItem.class);
+
+        Metadata metadata = metadataSources.buildMetadata();
+        SessionFactory sessionFactory = metadata.buildSessionFactory();
+
+        return sessionFactory.createEntityManager();
+    }
+}
--- src/main/java/org/example/shopping/Main.java
+++ src/main/java/org/example/shopping/Main.java
@@ -1,18 +1,9 @@
 package org.example.shopping;
 
-import org.example.shopping.db.entity.Order;
-import org.example.shopping.db.entity.OrderItem;
-import org.example.shopping.db.entity.Product;
-import org.example.shopping.db.entity.Review;
 import org.example.shopping.db.repository.OrderRepository;
 import org.example.shopping.db.repository.ProductRepository;
 import org.example.shopping.test.OrderEntityTest;
 import org.example.shopping.test.ProductEntityTest;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import javax.persistence.EntityManager;
 
@@ -23,23 +14,9 @@ public class Main implements AutoCloseable {
     private final ProductRepository productRepository;
 
     public Main() {
-        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                .configure("hibernate.cfg.xml")
-                .build();
-
-        MetadataSources metadataSources = new MetadataSources(serviceRegistry);
-        metadataSources.addAnnotatedClass(Order.class);
-        metadataSources.addAnnotatedClass(Review.class);
-        metadataSources.addAnnotatedClass(Product.class);
-        metadataSources.addAnnotatedClass(OrderItem.class);
-
-        Metadata metadata = metadataSources.buildMetadata();
-        SessionFactory sessionFactory = metadata.buildSessionFactory();
-
-        entityManager = sessionFactory.createEntityManager();
+        entityManager = HibernateEntityManagerBuilder.build();
         orderRepository = new OrderRepository(entityManager);
         productRepository = new ProductRepository(entityManager);
-
     }
 
     public void runEntityTests() {
